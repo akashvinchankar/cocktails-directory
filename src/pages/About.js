171fbe3efb297d8794cf486ad2664fc9bce31575
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-09-25 20:02:23 +0530

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        excepturi blanditiis magnam enim ullam asperiores nesciunt, voluptatem
+        inventore dolorem aliquam perferendis illum animi iure quas officia
+        temporibus quis beatae molestiae rem officiis omnis, earum labore quos
+        deleniti? Error, dolorem quaerat.
+      </p>
+    </section>
+  );
+};
+
+export default About;
